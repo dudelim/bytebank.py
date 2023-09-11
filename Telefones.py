28eeee617981e32bfc26b2234cb3eb3b8d487de1
Duda Melim <137728498+dudelim@users.noreply.github.com>
2023-09-11 18:09:30 -0300

    Telefones.py

--- Telefones.py
+++ Telefones.py
@@ -1,12 +1,11 @@
 import re
 
 class Telefones:
-
   def __init__(self, telefone):
     if self.valida_telefone(telefone):
       self.numero = telefone
     else:
-      raise ValueError("Número incorreto!")
+      raise ValueError("Número inválido!")
 
   def valida_telefone(self, telefone):
     padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
