123a360492b2a26606612d72951e69a891875408
Duda Melim <137728498+dudelim@users.noreply.github.com>
2023-09-11 18:51:36 -0300

    Datas.py

--- Datas.py
+++ Datas.py
@@ -1,17 +1,25 @@
 from datetime import datetime, timedelta
 
+
 class Datas:
+
   def __init__(self):
     self.momento_cadastro = datetime.today()
 
   def mes_cadastro(self):
-    meses_do_ano = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    meses_do_ano = [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+        "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ]
 
     mes_cadastro = self.momento_cadastro.month - 1
-      return meses_do_ano[mes_cadastro]
+    return meses_do_ano[mes_cadastro]
 
   def dia_semana(self):
-    dia_semana = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "sábado", "domingo"]
+    dia_semana_lista = [
+        "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira",
+        "Sexta-Feira", "Sábado", "Domingo"
+    ]
     dia_semana = self.momento_cadastro.weekday()
     return dia_semana_lista[dia_semana]
 
@@ -20,5 +28,15 @@ class Datas:
     return data_formatada
 
   def tempo_cadastro(self):
-    tempo_cadastro = (datetime.today() + timedelta(days=30)) - self.momento(cadastro)
+    tempo_cadastro = (datetime.today() +
+                      timedelta(days=30)) - self.momento_cadastro
     return tempo_cadastro
+
+
+if __name__ == "__main__":
+  data = Datas()
+
+  print("Mês de Cadastro:", data.mes_cadastro())
+  print("Dia da Semana de Cadastro:", data.dia_semana())
+  print("Data Formatada:", data.format_data())
+  print("Tempo de Cadastro:", data.tempo_cadastro())
