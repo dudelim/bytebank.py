69079eca7d43b48a9ecd813e3b90f41be7f4e8e1
Duda Melim <137728498+dudelim@users.noreply.github.com>
2023-09-11 18:22:05 -0300

    Datas.py

--- Datas.py
+++ Datas.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 
-class DatasBr:
+class Datas:
   def __init__(self):
     self.momento_cadastro = datetime.today()
 
   def mes_cadastro(self):
-    meses_do_ano = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+    meses_do_ano = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
     mes_cadastro = self.momento_cadastro.month - 1
       return meses_do_ano[mes_cadastro]
 
   def dia_semana(self):
+    dia_semana = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "sábado", "domingo"]
     dia_semana = self.momento_cadastro.weekday()
-    return dia_semana
+    return dia_semana_lista[dia_semana]
+
+  def format_data(self):
+    data_formatada = self.momento_cadastro.strftime("%d/%m/%Y %H:%M")
+    return data_formatada
