8f11c7a3bc5e99e33341e89debb5f4f1a87a7696
Duda Melim <137728498+dudelim@users.noreply.github.com>
2023-09-11 18:56:57 -0300

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from CPF_CNPJ import Documento
-from validate_docbr import CNPJ
+from Telefones import Telefones
+from Datas import Datas
+from CEP import BuscaEndereco
+import re
 
-#cpf = Cpf("15316264754")
-#print(cpf_um)
+#Exemplos: CPF E CNPJ
+documento1 = Documento.cria_documento("29894042015")  #Cria um CPF válido
+print(f"CPF: {documento1}")
 
-exemplo_cnpj = "35379838000112"
-exemplo_cpf = "32007832062"
+documento2 = Documento.cria_documento("13956549000104")  #Cria um CNPJ válido
+print(f"CNPJ: {documento2}")
 
-#cnpj = CNPJ()
-#print(cnpj.validate9exemplo_cnpj))
+#Exemplo: Telefone
+numero = Telefones("1234567891234")  #Cria um número de telefone válido
+print(numero.format_numero())
 
-documento = Documento.cria_documento(exemplo_cnpj)
-print(documento)
+#Exemplos: Datas
+data = Datas()
+print("Mês de Cadastro:", data.mes_cadastro())
+print("Dia da Semana de Cadastro:", data.dia_semana())
+print("Data:", data.format_data())
+print("Tempo de Cadastro:", data.tempo_cadastro())
+
+#Exemplo: CEP
+cep_digitado = input("Digite um CEP válido (apenas números): ")
+endereco = BuscaEndereco(cep_digitado)
+bairro, cidade, uf = endereco.acessa_CEP()
+
+if bairro and cidade and uf:
+  print("Bairro:", bairro)
+  print("Cidade:", cidade)
+  print("Estado:", uf)
+else:
+  print("CEP não encontrado.")
